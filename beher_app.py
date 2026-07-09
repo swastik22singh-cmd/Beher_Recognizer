a72dffa8ac6d9601f54e00567e62067c679ad2e4
swastik22singh-cmd <swastik22singh@gmail.com>
2026-07-09 23:08:32 +0530

    Update beher_app.py

--- beher_app.py
+++ beher_app.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 from finder import find_beher
 
-st.title("Hindi / Urdu Beher Recognizer")
+st.title(" 🖋️Hindi Beher Recognizer")
 
 text = st.text_area("Enter Shayari")
 
